f1846ce0b1abd178406cc520c779c96829958165
alaksandar_km <ebenzomarket@gmail.com>
2023-06-12 20:12:13 +0200

    add route, lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { Counter } from './components/Counter'
+import { Suspense } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
 import './index.scss'
 
 const App = () => {
 
     return (
         <div className='app'>
-            <p>test</p>
-            <Counter />
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 }
