4b3be6727fbe3453550be03e9ac3bb3c0665d22a
alaksandar_km <ebenzomarket@gmail.com>
2023-06-17 22:34:14 +0200

    styles, context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Suspense } from 'react'
+import { Suspense, useContext, useState } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
-import './index.scss'
+import { Theme, ThemeContext } from './theme/themeContext'
+import './styles/index.scss'
+import { useTheme } from './theme/useTheme'
+
 
 const App = () => {
 
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggle theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
